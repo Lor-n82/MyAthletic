24e14b557a24e36822d0e401a70bb00a81e26bd7
Loren Aparicio <loren.djkiller@gmail.com>
2018-01-26 14:12:29 +0100

    Many changes added

--- app/src/main/java/com/example/in2dm3_03/myathletic/Socio.java
+++ app/src/main/java/com/example/in2dm3_03/myathletic/Socio.java
@@ -1,7 +1,10 @@
 package com.example.in2dm3_03.myathletic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Socio extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class Socio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_socio);
+    }
+
+    public void onClick(View v){
+        Button boton=(Button)v;
+
+        if(boton.getId()==R.id.buttonLogin){
+            Intent i1=new Intent(this,pantalla_socio.class);
+            startActivity(i1);
+        }else if(boton.getId()==R.id.buttonRegistro){
+            Intent i1=new Intent(this,nuevo_registro.class);
+            startActivity(i1);
+        }
     }
 }
---
+++ app/src/main/java/com/example/in2dm3_03/myathletic/nuevo_registro.java
@@ -0,0 +1,13 @@
+package com.example.in2dm3_03.myathletic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class nuevo_registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_registro);
+    }
+}
