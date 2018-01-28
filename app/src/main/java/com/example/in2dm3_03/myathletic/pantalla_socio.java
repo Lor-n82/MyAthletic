5fe7cc0c920a2ee16b2ae211be53c3caf725ab3a
lor-n82 <loren.djkiller@gmail.com>
2018-01-28 18:44:49 +0100

    Activity "pantalla_socio" started

--- app/src/main/java/com/example/in2dm3_03/myathletic/pantalla_socio.java
+++ app/src/main/java/com/example/in2dm3_03/myathletic/pantalla_socio.java
@@ -7,13 +7,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class pantalla_socio extends AppCompatActivity {
 
-
+TextView socio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_socio);
+        socio=(TextView)findViewById(R.id.textViewNombreSocio);
+
+        socio.setText(getIntent().getStringExtra("Username"));
     }
 }
