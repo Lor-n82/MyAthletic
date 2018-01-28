71aa2bc7715977a8c37aa16c69452c17040351c8
lor-n82 <loren.djkiller@gmail.com>
2018-01-28 19:50:59 +0100

    Activity "pantalla_socio" finished
    Navigator doesn't load the page

---
+++ app/src/main/java/com/example/in2dm3_03/myathletic/NavegadorSocios.java
@@ -0,0 +1,32 @@
+package com.example.in2dm3_03.myathletic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class NavegadorSocios extends AppCompatActivity {
+    private String mUrl;
+    private WebView web;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navegador_socios);
+
+        mUrl = getIntent().getStringExtra("servicios");
+        web = (WebView) findViewById(R.id.navegadorSocios);
+
+        web.setWebViewClient(new MyWebViewClient());
+        WebSettings settings = web.getSettings();
+        web.loadUrl(mUrl);
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
--- app/src/main/java/com/example/in2dm3_03/myathletic/pantalla_socio.java
+++ app/src/main/java/com/example/in2dm3_03/myathletic/pantalla_socio.java
@@ -7,17 +7,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class pantalla_socio extends AppCompatActivity {
+    static final String NAVEGADOR = "servicio";
+    TextView socio;
 
-TextView socio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_socio);
-        socio=(TextView)findViewById(R.id.textViewNombreSocio);
+        socio = (TextView) findViewById(R.id.textViewNombreSocio);
 
         socio.setText(getIntent().getStringExtra("Username"));
     }
+
+    public void onClickSocio(View v) {
+        ImageView imagen = (ImageView) v;
+        Intent i1 = new Intent(this, NavegadorSocios.class);
+        if (imagen.getId() == R.id.imageButtonClasificacion) {
+            i1.putExtra(NAVEGADOR, "http://www.athletic-club.eus/cas/clasificacion/2017-18/athletic-liga.html");
+            startActivity(i1);
+        } else if (imagen.getId() == R.id.imageButtonPartidos) {
+            i1.putExtra(NAVEGADOR, "http://www.athletic-club.eus/cas/calendario/2017-18/athletic-liga.html");
+            startActivity(i1);
+        } else if (imagen.getId() == R.id.imageButtonTienda) {
+            i1.putExtra(NAVEGADOR, "https://www.athletic-club.eus/cas/tienda/new-balance_1-equipacion/36-356.html");
+            startActivity(i1);
+        }
+    }
 }
+
